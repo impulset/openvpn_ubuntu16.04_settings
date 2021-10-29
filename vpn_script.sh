bc1f2b38d0f03378f223866272d602268c36e7db
Pavel <69989483+sizykhpa@users.noreply.github.com>
2021-10-29 08:58:46 -0400

    Update vpn_script.sh

--- vpn_script.sh
+++ vpn_script.sh
@@ -24,4 +24,4 @@ source vars
 cd ~/openvpn-ca/keys
 sudo cp ca.crt server.crt server.key ta.key dh2048.pem /etc/openvpn
 
-gunzip -c /usr/share/doc/openvpn/examples/sample-config-files/server.conf.gz | sudo tee /etc/openvpn/server.conf
+cp server.conf /etc/openvpn/server.conf
